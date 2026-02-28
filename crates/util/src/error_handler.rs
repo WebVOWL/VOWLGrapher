da2d16cd80953201d0dc6fa8439e950989416b45
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2026-02-28 01:55:29 +0100

    Implement conversion functionality between other error types, ErrorRecords, and ServerFnError

--- crates/util/src/error_handler.rs
+++ crates/util/src/error_handler.rs
@@ -1,56 +1,32 @@
-// Use this to implement (de)encoding error types:
-// Encode: https://github.com/leptos-rs/leptos/blob/6dc8ad4bfa71c33bf67aac514204af5dcaf7a112/server_fn/src/error.rs#L264
-// Decode: https://github.com/leptos-rs/leptos/blob/6dc8ad4bfa71c33bf67aac514204af5dcaf7a112/server_fn/src/error.rs#L306
+#[cfg(not(feature = "server"))]
+use std::fmt::Write;
 
 use leptos::{
-    prelude::{FromServerFnError, ServerFnError, ServerFnErrorErr},
+    prelude::{FromServerFnError, IntoView, ServerFnError, ServerFnErrorErr},
     server_fn::{Decodes, Encodes, codec::RkyvEncoding, error::IntoAppError},
 };
+
+// use leptos_struct_table::*;
 use serde::{Deserialize, Serialize};
 
-/// Logs a server message at the error level.
-///
-/// Please use the `target` argument to define the error type. See Examples.
-///
-/// Returns a [`leptos::prelude::ServerFnError::ServerError<String>`]
-///
-/// # Examples
-///
-/// ```
-/// use vowlr_util::error_s;
-/// use leptos::prelude::ServerFnError;
-///
-/// let (err_info, port) = ("No connection", 22);
-///
-/// let s = error_s!("Error: {err_info} on port {port}");
-/// let s1 = error_s!(target: "serializer", "App Error: {err_info}, Port: {port}");
-///
-/// assert_eq!()
-/// ```
-#[macro_export]
-macro_rules! error_s {
-    // error!(target: "my_target", key1 = 42, key2 = true; "a {} event", "log")
-    // error!(target: "my_target", "a {} event", "log")
-    (target: $target:expr, $($arg:tt)+) => ({
-        $log::error!($($arg)+)
-        $leptos::prelude::ServerFnError::ServerError(
-                    $std::format_args!($target, $($arg)+)
-                )
-    });
-
-    // error!(key1 = 42, key2 = true; "a {} event", "log")
-    // error!("a {} event", "log")
-    ($($arg:tt)+) => ({
-        $log::error!($($arg)+)
-        $leptos::prelude::ServerFnError::ServerError(
-                    $std::format_args!($($arg)+)
-                )
-    });
-}
+#[cfg(feature = "server")]
+use tabled::{
+    Table, Tabled,
+    settings::{Settings, Style},
+};
 
 #[derive(
-    Debug, Copy, Clone, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Serialize, Deserialize,
+    Debug,
+    Copy,
+    Clone,
+    rkyv::Archive,
+    rkyv::Serialize,
+    rkyv::Deserialize,
+    Serialize,
+    Deserialize,
+    strum::Display,
 )]
+#[strum(serialize_all = "title_case")]
 pub enum ErrorSeverity {
     Critical,
     Severe,
@@ -60,30 +36,38 @@ pub enum ErrorSeverity {
     Unset,
 }
 
-impl std::fmt::Display for ErrorSeverity {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'e>) -> std::fmt::Result {
+impl ErrorSeverity {
+    // TODO: Work in progress. Pls don't remove format!
+    pub fn description(&self) -> String {
         match self {
-            Self::Critical => write!(
-                f,
+            Self::Critical => format!(
                 "an unrecoverable error which makes VOWL-R unusable (do not use the output of VOWL-R!)"
             ),
-            Self::Severe => write!(
-                f,
+            Self::Severe => format!(
                 "an error which highly disrupts the user experience (the output of VOWL-R is likely incorrect)"
             ),
-            Self::Medium => write!(f, "error desc goes here"),
-            Self::Low => write!(
-                f,
+            // TODO
+            Self::Medium => format!("error desc goes here"),
+            // TODO
+            Self::Low => format!(
                 "error desc goes here (part of the output of VOWL-R could be incorrect, but should be \"insignificant\")"
             ),
-            Self::Warning => write!(
-                f,
+            Self::Warning => format!(
                 "something happened which may reduce the user experience (but can otherwise be ignored)"
             ),
-            Self::Unset => write!(f, "unknown severity"),
+            Self::Unset => format!("unknown severity"),
         }
     }
 }
+
+// impl CellValue for ErrorSeverity {
+//     type RenderOptions = ();
+
+//     // #[allow(unused)]
+//     fn render_value(self, _options: Self::RenderOptions) -> impl IntoView {
+//         self.to_string()
+//     }
+// }
 
 #[derive(
     Debug,
@@ -108,13 +92,39 @@ pub enum ErrorType {
     #[strum(serialize = "GUI")]
     /// Errors related to the frontend GUI.
     Gui,
-    /// Errors without a type. Equivalent to a "500 Internal Server Error"
-    Generic,
+    /// Server errors without a type.
+    InternalServerError,
+    /// Client errors without a type.
+    ClientError,
 }
 
+// impl CellValue for ErrorType {
+//     type RenderOptions = ();
+
+//     fn render_value(self, _options: Self::RenderOptions) -> impl IntoView {
+//         self.to_string()
+//     }
+// }
+
 #[derive(
-    Debug, Clone, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Serialize, Deserialize,
+    Debug,
+    Clone,
+    rkyv::Archive,
+    rkyv::Serialize,
+    rkyv::Deserialize,
+    Serialize,
+    Deserialize,
+    // TableRow,
 )]
+#[cfg_attr(feature = "server", derive(Tabled))]
+// #[table(sortable, classes_provider = "TailwindClassesPreset")]
+// TODO: implement a leptos struct table looking like: https://datatables.net/
+/// The fundamental building block of the error handling system.
+///
+/// It stores the data of a single error event.
+///
+/// # Note
+/// Every error type in use should implement [`From<T> for ErrorRecord`].
 pub struct ErrorRecord {
     /// The severity of an error.
     ///
@@ -149,32 +159,99 @@ impl ErrorRecord {
             location,
         }
     }
+
+    #[cfg(feature = "server")]
+    /// Only available on the server.
+    pub fn format_records(records: &[ErrorRecord]) -> String {
+        let table_config = Settings::default().with(Style::modern_rounded());
+        Table::new(records).with(table_config).to_string()
+    }
+}
+
+impl From<ServerFnError> for ErrorRecord {
+    fn from(value: ServerFnError) -> Self {
+        let (error_type, message) = match value {
+            ServerFnError::WrappedServerError(_) => (
+                // TODO: Remove in Leptos v0.9
+                ErrorType::InternalServerError,
+                "deprecated WrappedServerError".to_string(),
+            ),
+            ServerFnError::Registration(e) => (ErrorType::InternalServerError, e),
+            ServerFnError::Request(e) => (ErrorType::ClientError, e),
+            ServerFnError::Response(e) => (ErrorType::InternalServerError, e),
+            ServerFnError::ServerError(e) => (ErrorType::InternalServerError, e),
+            ServerFnError::MiddlewareError(e) => (ErrorType::InternalServerError, e),
+            ServerFnError::Deserialization(e) => (ErrorType::ClientError, e),
+            ServerFnError::Serialization(e) => (ErrorType::ClientError, e),
+            ServerFnError::Args(e) => (ErrorType::InternalServerError, e),
+            ServerFnError::MissingArg(e) => (ErrorType::InternalServerError, e),
+        };
+
+        ErrorRecord::new(
+            ErrorSeverity::Unset,
+            error_type,
+            message,
+            #[cfg(debug_assertions)]
+            "N/A".to_string(),
+        )
+    }
+}
+
+impl From<ServerFnErrorErr> for ErrorRecord {
+    fn from(value: ServerFnErrorErr) -> Self {
+        let a: ServerFnError = value.into();
+        a.into()
+    }
+}
+
+#[cfg(feature = "server")]
+impl std::fmt::Display for ErrorRecord {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let table_config = Settings::default().with(Style::modern_rounded());
+        let table = Table::new([self]).with(table_config).to_string();
+        write!(f, "{}", table)
+    }
+}
+
+#[cfg(not(feature = "server"))]
+impl std::fmt::Display for ErrorRecord {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        #[cfg(debug_assertions)]
+        {
+            write!(
+                f,
+                "{} | {} | {} | {}",
+                self.severity, self.error_type, self.message, self.location
+            )
+        }
+
+        #[cfg(not(debug_assertions))]
+        {
+            write!(
+                f,
+                "{} | {} | {}",
+                self.severity, self.error_type, self.message
+            )
+        }
+    }
 }
 
 #[derive(
     Debug, Clone, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Serialize, Deserialize,
 )]
+/// The struct sent by the server when things go south.
+///
+/// # Note
+/// Every error type in use should implement [`From<T> for VOWLRServerError`].
 pub struct VOWLRServerError {
     pub records: Vec<ErrorRecord>,
 }
 
-impl<VOWLRServerError> FromServerFnError for ServerFnError<VOWLRServerError> {
+impl FromServerFnError for VOWLRServerError {
     type Encoder = RkyvEncoding;
 
     fn from_server_fn_error(value: ServerFnErrorErr) -> Self {
-        let (severity, error_type, message, location) = match value {
-            ServerFnErrorErr::Registration(e) => todo!(),
-            ServerFnErrorErr::UnsupportedRequestMethod(e) => todo!(),
-            ServerFnErrorErr::Request(e) => todo!(),
-            ServerFnErrorErr::ServerError(e) => todo!(),
-            ServerFnErrorErr::MiddlewareError(e) => todo!(),
-            ServerFnErrorErr::Deserialization(e) => todo!(),
-            ServerFnErrorErr::Serialization(e) => todo!(),
-            ServerFnErrorErr::Args(e) => todo!(),
-            ServerFnErrorErr::MissingArg(e) => todo!(),
-            ServerFnErrorErr::Response(e) => todo!(),
-        }
-
+        value.into()
     }
 
     fn ser(&self) -> leptos::server_fn::Bytes {
@@ -182,15 +259,68 @@ impl<VOWLRServerError> FromServerFnError for ServerFnError<VOWLRServerError> {
             Self::Encoder::encode(&Self::from_server_fn_error(
                 ServerFnErrorErr::Serialization(e.to_string()),
             ))
-            .expect(
-                "error serializing should success at least with the \
-                 Serialization error",
-            )
+            .expect("serializing should at least succeed with the serialization error type")
         })
     }
 
     fn de(data: leptos::server_fn::Bytes) -> Self {
         Self::Encoder::decode(data)
             .unwrap_or_else(|e| ServerFnErrorErr::Deserialization(e.to_string()).into_app_error())
+    }
+}
+
+impl From<ServerFnError> for VOWLRServerError {
+    fn from(value: ServerFnError) -> Self {
+        let record: ErrorRecord = value.into();
+        record.into()
+    }
+}
+
+impl From<ServerFnErrorErr> for VOWLRServerError {
+    fn from(value: ServerFnErrorErr) -> Self {
+        let record: ErrorRecord = value.into();
+        record.into()
+    }
+}
+
+impl From<ErrorRecord> for VOWLRServerError {
+    fn from(value: ErrorRecord) -> Self {
+        VOWLRServerError {
+            records: vec![value],
+        }
+    }
+}
+
+impl From<Vec<ErrorRecord>> for VOWLRServerError {
+    fn from(value: Vec<ErrorRecord>) -> Self {
+        VOWLRServerError { records: value }
+    }
+}
+
+#[cfg(feature = "server")]
+impl std::fmt::Display for VOWLRServerError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", ErrorRecord::format_records(&self.records))
+    }
+}
+
+#[cfg(not(feature = "server"))]
+impl std::fmt::Display for VOWLRServerError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        #[cfg(debug_assertions)]
+        {
+            writeln!(f, "Severity | Error Type | Message | Location")?;
+        }
+
+        #[cfg(not(debug_assertions))]
+        {
+            writeln!(f, "Severity | Error Type | Message")?;
+        }
+
+        let mut buffer = String::new();
+        for record in self.records.iter() {
+            writeln!(buffer, "{}", record)?;
+        }
+        write!(f, "{buffer}")
     }
 }

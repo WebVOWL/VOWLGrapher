910f3499c76199f9c48dfe6d31ef6e8ca68f7433
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2026-04-12 03:29:47 +0200

    Cleanup

--- src/errors.rs
+++ src/errors.rs
@@ -6,9 +6,9 @@ use vowlr_util::prelude::{ErrorRecord, ErrorSeverity, ErrorType, VOWLRError, get
 pub enum ClientErrorKind {
     /// An error raised when an unexpected value was received from JS-land.
     JavaScriptError(String),
-    /// Errors related to the graph renderer (i.e. ``WasmGrapher``)
+    /// Errors related to the graph renderer.
     RenderError(String),
-    /// Errors related to file upload
+    /// Errors related to file upload.
     FileUploadError(String),
     /// An error raised when the event handler fails to send or receive events.
     EventHandlingError(String),
@@ -52,52 +52,6 @@ impl From<ClientErrorKind> for VOWLRError {
         a.into()
     }
 }
-
-// #[derive(Debug)]
-// pub struct VOWLRClientError {
-//     /// The contained error type.
-//     inner: ClientErrorKind,
-//     /// The error's location in the source code.
-//     location: &'static Location<'static>,
-// }
-// impl std::fmt::Display for VOWLRClientError {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         write!(f, "{:?}", self.inner)
-//     }
-// }
-
-// impl From<ClientErrorKind> for VOWLRClientError {
-//     #[track_caller]
-//     fn from(error: ClientErrorKind) -> Self {
-//         VOWLRClientError {
-//             inner: error,
-//             location: Location::caller(),
-//         }
-//     }
-// }
-
-// impl From<VOWLRClientError> for ErrorRecord {
-//     fn from(value: VOWLRClientError) -> Self {
-//         let (message, error_type, severity) = match value.inner {
-//             ClientErrorKind::JavaScriptError(e) => (e, ErrorType::Gui, ErrorSeverity::Error),
-//             ClientErrorKind::RenderError(e) => (e, ErrorType::Renderer, ErrorSeverity::Critical),
-//         };
-//         ErrorRecord::new(
-//             severity,
-//             error_type,
-//             message,
-//             #[cfg(debug_assertions)]
-//             Some(value.location.to_string()),
-//         )
-//     }
-// }
-
-// impl From<VOWLRClientError> for VOWLRError {
-//     fn from(value: VOWLRClientError) -> Self {
-//         let a: ErrorRecord = value.into();
-//         a.into()
-//     }
-// }
 
 #[derive(Debug, Copy, Clone)]
 pub struct ErrorLogContext {

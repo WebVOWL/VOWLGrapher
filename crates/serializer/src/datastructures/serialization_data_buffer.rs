4e36f9e1794b1786a01ba06205ed202b65ed53a6
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2026-04-22 23:39:14 +0200

    Add converter for metadata

--- crates/serializer/src/datastructures/serialization_data_buffer.rs
+++ crates/serializer/src/datastructures/serialization_data_buffer.rs
@@ -1,11 +1,14 @@
 use crate::{
     datastructures::{
-        ArcEdge, ArcLockRestrictionState, ArcTriple, graph_metadata_buffer::GraphMetadataBuffer,
-        index::TermIndex,
+        ArcEdge, ArcLockRestrictionState, ArcTriple, DocumentBase,
+        graph_metadata_buffer::GraphMetadataBuffer, index::TermIndex,
     },
     errors::{SerializationError, SerializationErrorKind},
+    serializer_util::trim_tag_circumfix,
 };
-use grapher::prelude::{Characteristic, ElementType, GraphDisplayData, OwlEdge, OwlType};
+use grapher::prelude::{
+    Characteristic, ElementType, GraphDisplayData, GraphMetadata, OwlEdge, OwlType,
+};
 use log::debug;
 use std::{
     collections::{HashMap, HashSet},
@@ -25,7 +28,7 @@ pub struct SerializationDataBuffer {
     /// Maps terms to integer ids and vice-versa.
     ///
     /// Reduces memory usage and allocations.
-    pub term_index: TermIndex,
+    pub term_index: Arc<TermIndex>,
     /// Stores all resolved node elements.
     ///
     /// The key is a term's corresponding id.
@@ -100,13 +103,18 @@ pub struct SerializationDataBuffer {
     /// The base IRI of the document.
     ///
     /// For instance: `http://purl.obolibrary.org/obo/envo.owl`
-    pub document_base: Arc<RwLock<Option<Arc<String>>>>,
+    pub document_base: Arc<RwLock<Option<DocumentBase>>>,
     /// Data not visualized in the graph.
     pub metadata: GraphMetadataBuffer,
 }
 impl SerializationDataBuffer {
     pub fn new() -> Self {
-        Self::default()
+        let term_index: Arc<TermIndex> = TermIndex::new().into();
+        Self {
+            metadata: GraphMetadataBuffer::new(term_index.clone()),
+            term_index,
+            ..Default::default()
+        }
     }
 
     /// Unpack the predicate term id of the triple.
@@ -122,11 +130,6 @@ impl SerializationDataBuffer {
         }
     }
 
-    #[expect(
-        clippy::significant_drop_tightening,
-        reason = "this method clears most buffers and is expected to be called at the end of processing. 
-        Thus, keeping locks longer than necessary doesn't matter"
-    )]
     /// Converts [`self`] into [`GraphDisplayData`].
     ///
     /// Works like [`TryFrom`] except it also returns non-critical errors in [`Result::Ok`].
@@ -142,6 +145,32 @@ impl SerializationDataBuffer {
         // Maps an RDF term's corresponding id to a [`GraphDisplayData`] index.
         let mut inverse_edge_indices: HashMap<usize, usize> = HashMap::new();
 
+        self.convert_graph_data(
+            &mut display_data,
+            &mut failed,
+            &mut iricache,
+            &mut inverse_edge_indices,
+        )?;
+        self.convert_metadata(&mut display_data, &mut failed, &iricache)?;
+
+        if failed.is_empty() {
+            Ok((display_data, None))
+        } else {
+            Ok((display_data, Some(failed.into())))
+        }
+    }
+
+    #[expect(
+        clippy::significant_drop_tightening,
+        reason = "this method runs single-threaded"
+    )]
+    fn convert_graph_data(
+        &self,
+        display_data: &mut GraphDisplayData,
+        failed: &mut Vec<ErrorRecord>,
+        iricache: &mut HashMap<usize, usize>,
+        inverse_edge_indices: &mut HashMap<usize, usize>,
+    ) -> Result<(), SerializationError> {
         let mut label_buffer = self.label_buffer.write()?;
         let mut node_element_buffer = self.node_element_buffer.write()?;
         for (term_id, element) in take(&mut *node_element_buffer) {
@@ -270,12 +299,191 @@ impl SerializationDataBuffer {
                 display_data.individual_counts.insert(*idx, count);
             }
         }
+        Ok(())
+    }
 
-        if failed.is_empty() {
-            Ok((display_data, None))
-        } else {
-            Ok((display_data, Some(failed.into())))
+    #[expect(
+        clippy::significant_drop_tightening,
+        reason = "this method runs single-threaded"
+    )]
+    fn convert_metadata(
+        &self,
+        display_data: &mut GraphDisplayData,
+        failed: &mut Vec<ErrorRecord>,
+        iricache: &HashMap<usize, usize>,
+    ) -> Result<(), SerializationError> {
+        let mut metadata = GraphMetadata::new();
+
+        metadata.document_base = self
+            .document_base
+            .read()?
+            .clone()
+            .map_or_else(String::new, |docbase| docbase.base);
+        metadata.description = {
+            let value = self.document_base.read()?.clone();
+            if let Some(docbase) = value {
+                if let Ok(base_term_id) = self.term_index.get_id(&docbase.base_term) {
+                    self.metadata
+                        .comment_buffer
+                        .read()?
+                        .get(&base_term_id)
+                        .map_or_else(String::new, |comment_term_id| {
+                            self.term_index
+                                .get(*comment_term_id)
+                                .map_or_else(|e| e.to_string(), |term| term.to_string())
+                        })
+                } else {
+                    let msg = format!(
+                        "Failed to create ontology description: Term id for document base '{}' not found in term index",
+                        docbase.base_term
+                    );
+                    debug!("{msg}");
+                    failed.push(SerializationErrorKind::TermIndexError(msg.clone()).into());
+                    msg
+                }
+            } else {
+                String::new()
+            }
+        };
+        metadata.title = {
+            let value = self.document_base.read()?.clone();
+            if let Some(docbase) = value {
+                if let Ok(base_term_id) = self.term_index.get_id(&docbase.base_term) {
+                    match self.label_buffer.read()?.get(&base_term_id) {
+                        Some(Some(label)) => label.clone(),
+                        Some(None) => {
+                            // No label declared
+                            let msg = "Ontology title not found in ontology.".to_string();
+
+                            debug!("{msg}");
+                            failed.push(SerializationErrorKind::SerializationWarning(msg).into());
+                            String::new()
+                        }
+                        None => {
+                            // No label found in buffer
+                            let msg = "Ontology title not found in label buffer".to_string();
+                            debug!("{msg}");
+                            failed.push(
+                                SerializationErrorKind::SerializationWarning(msg.clone()).into(),
+                            );
+                            msg
+                        }
+                    }
+                } else {
+                    let msg = format!(
+                        "Failed to create ontology title: Term id for document base '{}' not found in term index",
+                        docbase.base_term
+                    );
+                    debug!("{msg}");
+                    failed.push(SerializationErrorKind::TermIndexError(msg.clone()).into());
+                    msg
+                }
+            } else {
+                String::new()
+            }
+        };
+        let mut author_buffer = self.metadata.author_buffer.write()?;
+        for (term_id, author) in take(&mut *author_buffer) {
+            // TODO: Implement
         }
+        let mut comment_buffer = self.metadata.comment_buffer.write()?;
+        for (term_id, comment_term_id) in take(&mut *comment_buffer) {
+            if let Some(term_idx) = iricache.get(&term_id) {
+                let comment = self
+                    .term_index
+                    .get(comment_term_id)
+                    .map_or_else(|e| e.to_string(), |term| term.to_string());
+                if let Some(old_comment) = metadata.comments.insert(*term_idx, comment.clone()) {
+                    let term = self
+                        .term_index
+                        .get(term_id)
+                        .map_or_else(|e| e.to_string(), |term| term.to_string());
+                    let msg = format!(
+                        "Overriding comment '{old_comment}' for term '{term}' with new comment '{comment}'"
+                    );
+                    debug!("{msg}");
+                    failed.push(SerializationErrorKind::SerializationWarning(msg).into());
+                }
+            } else {
+                let msg = match self.term_index.get(term_id) {
+                    Ok(term) => {
+                        format!("Failed to map comment: Term '{term}' not found in iricache")
+                    }
+                    Err(e) => {
+                        format!("Failed to map comment: Term '{e}' not found in iricache")
+                    }
+                };
+                debug!("{msg}");
+            }
+        }
+        let mut defined_by_buffer = self.metadata.defined_by_buffer.write()?;
+        for (term_id, defined_by_term_id) in take(&mut *defined_by_buffer) {
+            let maybe_term_idx = iricache.get(&term_id);
+            let maybe_defined_by_term = self.term_index.get(defined_by_term_id);
+
+            match (maybe_term_idx, maybe_defined_by_term) {
+                (Some(term_idx), Ok(defined_by_term)) => {
+                    let new_defined_by = trim_tag_circumfix(&defined_by_term.to_string());
+                    if let Some(old_defined_by) = metadata
+                        .is_defined_by
+                        .insert(*term_idx, new_defined_by.clone())
+                    {
+                        let term = self
+                            .term_index
+                            .get(term_id)
+                            .map_or_else(|e| e.to_string(), |term| term.to_string());
+                        let msg = format!(
+                            "Overriding isDefinedBy '{old_defined_by}' for term '{term}' with new isDefinedBy '{new_defined_by}'"
+                        );
+                        debug!("{msg}");
+                        failed.push(SerializationErrorKind::SerializationWarning(msg).into());
+                    }
+                }
+                (None, Ok(_)) => {}
+                (Some(_) | None, Err(e)) => {
+                    failed.push(e.into());
+                }
+            }
+        }
+        let mut see_also_buffer = self.metadata.see_also_buffer.write()?;
+        for (term_id, see_also) in take(&mut *see_also_buffer) {
+            // TODO: Implement
+        }
+        metadata.version_iri = Some(self.metadata.version_iri.read()?.map_or_else(
+            String::new,
+            |version_term_id| {
+                self.term_index
+                    .get(version_term_id)
+                    .map_or_else(|e| e.to_string(), |term| term.to_string())
+            },
+        ));
+        metadata.prior_version = Some(self.metadata.prior_version.read()?.map_or_else(
+            String::new,
+            |version_term_id| {
+                self.term_index
+                    .get(version_term_id)
+                    .map_or_else(|e| e.to_string(), |term| term.to_string())
+            },
+        ));
+        metadata.incompatible_with = Some(self.metadata.prior_version.read()?.map_or_else(
+            String::new,
+            |version_term_id| {
+                self.term_index
+                    .get(version_term_id)
+                    .map_or_else(|e| e.to_string(), |term| term.to_string())
+            },
+        ));
+        metadata.backward_compatible_with = Some(self.metadata.prior_version.read()?.map_or_else(
+            String::new,
+            |version_term_id| {
+                self.term_index
+                    .get(version_term_id)
+                    .map_or_else(|e| e.to_string(), |term| term.to_string())
+            },
+        ));
+
+        display_data.graph_metadata = metadata;
+        Ok(())
     }
 }
 
@@ -290,7 +498,7 @@ impl Display for SerializationDataBuffer {
                 .read()
                 .unwrap_or_else(std::sync::PoisonError::into_inner)
                 .clone()
-                .unwrap_or_default()
+                .map_or_else(String::new, |docbase| docbase.base)
         )?;
         writeln!(f, "\tnode_element_buffer:")?;
         for (term_id, element) in self
@@ -455,6 +663,7 @@ impl Display for SerializationDataBuffer {
         // Not needed as it's displayed by the serializer
         // writeln!(f, "\tfailed_buffer:")?;
         // writeln!(f, "{}", ErrorRecord::format_records(&self.failed_buffer))?;
+        write!(f, "{}", self.metadata)?;
         writeln!(f, "}}")
     }
 }

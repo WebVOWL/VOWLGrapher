78e601fb1552a7af8cde54af35b82aa60307118f
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2026-04-22 23:35:56 +0200

    Log import res with info level

--- crates/database/src/store.rs
+++ crates/database/src/store.rs
@@ -360,7 +360,7 @@ impl VOWLGrapherStore {
         root_base: ImportBase,
     ) -> Result<(Vec<Quad>, Option<VOWLGrapherError>), VOWLGrapherStoreError> {
         if !VOWLGRAPHER_ENVIRONMENT.resolve_imports {
-            debug!("Import resolution disabled via VOWLGRAPHER_RESOLVE_IMPORTS");
+            info!("Import resolution disabled via VOWLGRAPHER_RESOLVE_IMPORTS");
             return Ok((root_quads, None));
         }
 

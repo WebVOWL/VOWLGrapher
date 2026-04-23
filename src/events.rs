2c33062fa8771411bd4dc880baac8dcc0b9fcc13
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2026-04-23 12:42:42 +0000

    Implement event API for selection details

--- src/events.rs
+++ src/events.rs
@@ -1,25 +1,38 @@
 use crate::errors::{ClientErrorKind, ErrorLogContext};
 use grapher::prelude::{EVENT_DISPATCHER, GUIEvent};
-use leptos::prelude::expect_context;
-use log::info;
+use leptos::prelude::{RwSignal, Update, expect_context};
+
+#[derive(Clone, Copy)]
+pub struct EventContext {
+    pub show_metadata: RwSignal<Option<usize>>,
+}
+
+impl Default for EventContext {
+    fn default() -> Self {
+        Self {
+            show_metadata: RwSignal::new(None),
+        }
+    }
+}
+
+impl EventContext {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
 
 pub struct EventHandler;
 
 impl EventHandler {
     pub async fn handle_event() {
+        let EventContext { show_metadata } = expect_context::<EventContext>();
         loop {
             match EVENT_DISPATCHER.gui_read_chan.recv().await {
-                Ok(event) => {
-                    match event {
-                        GUIEvent::ShowMetadata(idx) => {
-                            // call relevant signal
-                            info!("{idx}");
-                        }
-                        GUIEvent::HideMetadata() => {
-                            // call relevant signal
-                        }
+                Ok(event) => match event {
+                    GUIEvent::ShowMetadata(idx) => {
+                        show_metadata.update(|v| *v = idx);
                     }
-                }
+                },
                 Err(e) => {
                     let error_context = expect_context::<ErrorLogContext>();
                     error_context.push(ClientErrorKind::EventHandlingError(e.to_string()).into());

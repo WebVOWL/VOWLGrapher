9bcda02b7ecaa7d3ae0ba23a0f6533a239185a82
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2026-05-05 23:38:31 +0200

    WIP on query assembly

--- crates/sparql_queries/src/assembly.rs
+++ crates/sparql_queries/src/assembly.rs
@@ -1,7 +1,12 @@
+mod query_regex;
 use crate::snippets::SparqlSnippet;
 use crate::{prelude::GENERAL_SNIPPETS, snippets::void::VOID};
 use grapher::prelude::ElementType;
+use indexmap::IndexSet;
+use log::info;
+use regex::Regex;
 use std::collections::HashMap;
+use std::fmt::Write;
 
 // TODO: Remove when automatic prefix fetching is implemented.
 pub const DEFAULT_PREFIXES: [&str; 8] = [
@@ -14,6 +19,10 @@ pub const DEFAULT_PREFIXES: [&str; 8] = [
     "dc: <http://purl.org/dc/elements/1.1/>",
     "dcterms: <http://purl.org/dc/terms/>",
 ];
+
+static VAR_REGEX: &str = r"[?$]([a-zA-Z_][a-zA-Z0-9_\u00B7\u0300-\u036F\u203F-\u2040]*)";
+static QUERY_VAR_REGEX: &str = r"(?:SELECT|select) ([?$].*|\*)(?:WHERE|where)?";
+// static QUERY_VAR_REGEX: &str = r"(?:SELECT|select) (?<first>[?$].*|\*)(?:WHERE|where)?";
 
 /// Compiles snippets of SPARQL code into full-fledged SPARQL queries.
 pub struct QueryAssembler;
@@ -76,22 +85,64 @@ impl QueryAssembler {
         Self::assemble_query(&DEFAULT_PREFIXES.into(), &snippets)
     }
 
+    #[expect(clippy::unwrap_used, reason = "Testing. Remove on sight")]
     /// Construct a custom SPARQL query based on the query inserted by the user in the `UI(query_menu)`
-    pub fn assemble_custom_query(user_query: &str) -> String {
+    pub fn assemble_user_query(user_query: &str) -> String {
+        info!("{user_query}");
+
         let prefixes = DEFAULT_PREFIXES
             .iter()
             .map(|item| format!("PREFIX {item}"))
             .collect::<Vec<_>>()
             .join("\n");
 
+        let query_var_re = Regex::new(VAR_REGEX).unwrap();
+
+        // NEVER remove elements from the IndexSet. Doing so breaks ordering!
+        // The variables to use in CONSTRUCT.
+        let query_variables = {
+            let mut q_set = IndexSet::new();
+            let re = Regex::new(QUERY_VAR_REGEX).unwrap();
+
+            for (_, [c]) in re.captures_iter(user_query).map(|c| c.extract()) {
+                query_var_re.find_iter(c).for_each(|m| {
+                    q_set.insert(m.as_str());
+                });
+            }
+
+            q_set
+        };
+
+        info!("{query_variables:?}");
+
+        let variable_type_snippet = query_variables
+            .iter()
+            .map(|v| format!("OPTIONAL {{ {v} rdf:type {v}_type }}"))
+            .collect::<Vec<_>>()
+            .join("\n");
+
+        let construct_type_snippet = query_variables
+            .iter()
+            .map(|v| format!("{v} rdf:type {v}_type"))
+            .collect::<Vec<_>>()
+            .join(". \n");
+
+        let query_variable_snippet =
+            query_variables
+                .iter()
+                .fold(String::new(), |mut buffer, var| {
+                    // SAFETY: writing strings is infallible
+                    // https://doc.rust-lang.org/stable/src/alloc/string.rs.html#2879
+                    let _ = write!(buffer, "{var}");
+                    buffer
+                });
+
         format!(
             r"
             {prefixes}
             CONSTRUCT {{
-                ?s ?p ?o .
-                ?s rdf:type ?typeS .
-                ?p rdf:type ?typeP .
-                ?o rdf:type ?typeO .
+                {query_variable_snippet} .
+                {construct_type_snippet}
             }}
             WHERE {{
                 GRAPH <{{GRAPH_IRI}}> {{
@@ -104,15 +155,12 @@ impl QueryAssembler {
                     {{
                         {{ {user_query} }}
 
-                        ?s ?p ?o .
-                        OPTIONAL {{ ?s rdf:type ?typeS }}
-                        OPTIONAL {{ ?p rdf:type ?typeP }}
-                        OPTIONAL {{ ?o rdf:type ?typeO }}
+                        {query_variable_snippet} .
+                        {variable_type_snippet}
                         
                     }}
                 }}
             }}
-            ORDER BY ?weight
             "
         )
     }
